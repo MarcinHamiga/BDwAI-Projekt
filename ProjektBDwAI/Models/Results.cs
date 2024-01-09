3973ccfa7cf7dd51553aba49bff3682ebd75c701
Kacper Słupski <kacperslupski02@gmail.com>
2024-01-09 16:30:50 +0100

    Dodano modele i ich nazwy

---
+++ ProjektBDwAI/Models/Results.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjektBDwAI.Models
+{
+    public class Results
+    {
+        public int Id { get; set; }
+        public int SurveyId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateFilled { get; set; }
+    }
+}
---
+++ ProjektBDwAI/Models/UserResults.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjektBDwAI.Models
+{
+    public class UserResults
+    {
+        public int Id { get; set; }
+        public int ResultId { get; set; }
+        public int QuestionId { get; set; }
+        public required string AnswerText { get; set; }
+        public required string SelectedAnswer { get; set; }
+    }
+}
